77617b73fb280ec485be01de728fb6539b088dad
anissmbk <aniss090.mbarki@gmail.com>
2019-12-22 01:33:04 +0100

    update

--- src/main/java/com/example/ebankingspg/java/Controller/TransactionController.java
+++ src/main/java/com/example/ebankingspg/java/Controller/TransactionController.java
@@ -10,6 +10,7 @@ import com.example.ebankingspg.java.request.CreateRecipientRequest;
 import com.example.ebankingspg.java.request.CreateTransactionRequest;
 import com.example.ebankingspg.java.response.AccountResponse;
 import com.example.ebankingspg.java.response.StringResponse;
+import com.example.ebankingspg.java.response.TransactionDetailsResponse;
 import com.example.ebankingspg.java.services.TransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
@@ -59,5 +60,54 @@ public class TransactionController {
     public ResponseEntity<?> getBadTransactions() throws Exception {
         List<Transaction> transactions = transactionRepository.findByActiveIsFalse();
         return ResponseEntity.ok(transactions);
+    }
+
+    //meryem
+    @GetMapping(produces = "application/json")
+    @RequestMapping(value = "/transaction_details", method = RequestMethod.GET)
+    public ResponseEntity<?> getTransactionDetails(@RequestParam String id) throws Exception {
+        Optional<Transaction> transaction = transactionRepository.findById(Long.parseLong(id));
+        transaction.orElseThrow(() -> new Exception("transaction not found"));
+        Transaction transaction1=transaction.get();
+        String recipientRib;
+        if(transaction1.getAccountTarget()==null){
+            recipientRib=transaction1.getRecipientOutOfBank();
+        }else{
+            recipientRib=transaction1.getAccountTarget().getRib();
+        }
+        Account accountTarget = transaction1.getAccountTarget();
+        if(accountTarget == null){
+            return ResponseEntity.ok(new TransactionDetailsResponse(transaction1,transaction1.getAccount().getRib(),
+                    recipientRib ,null));
+        }else{
+            return ResponseEntity.ok(new TransactionDetailsResponse(transaction1,transaction1.getAccount().getRib(),
+                    recipientRib ,transaction1.getAccountTarget().getClient()));
+        }
+    }
+
+    @GetMapping(produces = "application/json")
+    @RequestMapping(value = "/transaction_manager/valide_transaction",method = RequestMethod.POST)
+    public ResponseEntity<?> validateTransaction(@RequestBody long id) throws Exception {
+        Optional<Transaction> transaction = transactionRepository.findById(id);
+        Transaction transaction1=transaction.get();
+        transaction1.setActive(true);
+        Account account = transaction1.getAccountTarget();
+        account.setBalance(account.getBalance()+transaction1.getAmount());
+        accountRepository.save(account);
+        transactionRepository.save(transaction1);
+        return ResponseEntity.ok(new StringResponse("transaction validated successfully"));
+    }
+
+    @GetMapping(produces = "application/json")
+    @RequestMapping(value = "/transaction_manager/remove_transaction",method = RequestMethod.POST)
+    public ResponseEntity<?> removeTransaction(@RequestBody long id) throws Exception {
+        Optional<Transaction> transaction = transactionRepository.findById(id);
+        Transaction transaction1=transaction.get();
+        //return money lkhona lmsskin
+        Account account =transaction1.getAccount();
+        account.setBalance(account.getBalance()+transaction1.getAmount());
+        accountRepository.save(account);
+        transactionRepository.delete(transaction1);
+        return ResponseEntity.ok(new StringResponse("Transaction removed successfully"));
     }
 }
